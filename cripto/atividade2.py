b6ec188814496894a7c105d65bd0a8c144749c47
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-06-16 14:28:05 -0300

    fazendo com que a chave para a cifra de deslocamento seja gera aleatoriamente

--- cripto/atividade2.py
+++ cripto/atividade2.py
@@ -1,14 +1,15 @@
+import random
+
 def cripto(m,k ):
     textoCripto= []
-
     for letra in m:
         valor= (ord(letra) - ord("a") + k) % 26 + ord("a") 
         textoCripto.append(chr(valor))
     textoCripto="".join(textoCripto)
     return textoCripto
     
+print(cripto("",random.randrange(1,26)))
 
-textoCripto= cripto("fernandaechataz", 3)
 
 
 
