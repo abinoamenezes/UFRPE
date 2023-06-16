fea120117e62efee52960bf6ca5578b32b94adaf
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-06-16 16:41:46 -0300

    add function that make cleaning in the text

--- cripto/atividade2.py
+++ cripto/atividade2.py
@@ -1,14 +1,24 @@
 import random
 
+def cleaning (text):
+    textoLimpo=[]
+    for c in text:
+        if c.isalpha():
+            textoLimpo.append(c.lower())
+    textoLimpo="".join(textoLimpo)
+    return  textoLimpo
+
+
 def cripto(m,k ):
     textoCripto= []
     for letra in m:
         valor= (ord(letra) - ord("a") + k) % 26 + ord("a") 
         textoCripto.append(chr(valor))
     textoCripto="".join(textoCripto)
+    textoCripto= textoCripto.upper()
     return textoCripto
     
-print(cripto("",random.randrange(1,26)))
+print(cripto(cleaning("Olá, me chamo fulano da Silva, moro na rua Alfredo Alves.... 888888888 ++++++ +++++ ++++ +++"),random.randrange(1,26)))
 
 
 
