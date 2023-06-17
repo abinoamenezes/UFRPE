6884095eab84e0904e0f7460ae2c5f13226f8653
Abinoã Menezes <109997516+abinoamenezes@users.noreply.github.com>
2023-06-17 15:29:03 -0300

    melhorando a limpeza, fazendo com que os acentos sejam retirados

--- cripto/atividade1.py
+++ cripto/atividade1.py
@@ -1,9 +1,12 @@
+from unidecode import unidecode #essa biblioteca retira os acentos das letras. Precisa ser instalada pelo PIP
+
 def cleaning (text):
     textoLimpo=[]
     for c in text:
         if c.isalpha():
             textoLimpo.append(c.lower())
     textoLimpo="".join(textoLimpo)
+    textoLimpo=unidecode(textoLimpo)
     return "texto limpo: " + textoLimpo
    
 print(cleaning(input("Digite aqui o texto que será limpado: ")))
